9bbd2cb9685c30557920d81c1ea8c6482f71930f
BreakingFuse <BreakingFuse@hotmail.com>
2020-11-20 10:51:30 -0600

    Fractured Object Script and Colliders

---
+++ Assets/Scripts/FractureObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FractureObject : MonoBehaviour
+{
+    public GameObject fractured;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Instantiate(fractured, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+        
+    }
+
+}
